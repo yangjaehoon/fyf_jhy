a78a880d806ae59f9b11701f0a19b556c9d5c625
Jaehoon Yang <lljhms@naver.com>
2023-09-05 17:11:41 +0900

    personal post

--- fillyourfestival/lib/screen/main/tab/home/artist_page/w_main_image_swiper.dart
+++ fillyourfestival/lib/screen/main/tab/home/artist_page/w_main_image_swiper.dart
@@ -1,9 +1,6 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
-import 'package:card_swiper/card_swiper.dart';
-
-import '../../../../../common/constant/app_colors.dart';
 
 class MainImageSwiper extends StatefulWidget {
   const MainImageSwiper({super.key});
@@ -22,11 +19,33 @@ class _MainImageSwiperState extends State<MainImageSwiper> {
   ];
 
   final PageController _pageController = PageController(
-    viewportFraction: 0.6,
+    viewportFraction: 0.55,
   );
 
+  int _currentPage = 0;
 
-  int currentIndex = 0;
+  ValueNotifier<double> _scroll = ValueNotifier(0.0);
+
+  void _onPageChanged(int newPage) {
+    setState(() {
+      _currentPage = newPage;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController.addListener(() {
+      if (_pageController.page == null) return;
+      _scroll.value = _pageController.page!;
+    });
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,7 +53,29 @@ class _MainImageSwiperState extends State<MainImageSwiper> {
       height: 300,
       child: Stack(
         children: [
+          AnimatedSwitcher(
+            duration: Duration(milliseconds: 500),
+            child: Container(
+              key: ValueKey(_currentPage),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(ftvArtImgList[_currentPage]),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: BackdropFilter(
+                filter: ImageFilter.blur(
+                  sigmaX: 5,
+                  sigmaY: 5,
+                ),
+                child: Container(
+                  color: Colors.black.withOpacity(0.5),
+                ),
+              ),
+            ),
+          ),
           PageView.builder(
+            onPageChanged: _onPageChanged,
             controller: _pageController,
             itemCount: ftvArtImgList.length,
             //scrollDirection: Axis.horizontal,
@@ -42,14 +83,36 @@ class _MainImageSwiperState extends State<MainImageSwiper> {
               return Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Container(
-                    height: 300,
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          image: AssetImage(ftvArtImgList[index])
-                        )
-                      ),
-                  )
+                  ValueListenableBuilder(
+                    valueListenable: _scroll,
+                    builder: (context, scroll, child) {
+                      final difference = (scroll - index).abs();
+                      final scale = 1 - (difference * 0.2);
+                      return Transform.scale(
+                        scale: scale,
+                        child: Container(
+                          height: 254.5,
+                          width: 180,
+                          decoration: BoxDecoration(
+                            color: Colors.red,
+                            borderRadius: BorderRadius.circular(10),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.4),
+                                blurRadius: 10,
+                                spreadRadius: 2,
+                                offset: Offset(0, 8),
+                              ),
+                            ],
+                            image: DecorationImage(
+                              image: AssetImage(ftvArtImgList[index]),
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
                 ],
               );
             },
--- fillyourfestival/lib/screen/main/tab/home/w_concert_list_swiper.dart
+++ fillyourfestival/lib/screen/main/tab/home/w_concert_list_swiper.dart
@@ -19,67 +19,105 @@ class _ConcertListSwiperState extends State<ConcertListSwiper> {
     'assets/image/poster/waterbomb_poster.jpg',
   ];
 
+  final PageController _pageController = PageController(
+    viewportFraction: 0.55,
+  );
+
+  int _currentPage = 0;
+
+  ValueNotifier<double> _scroll = ValueNotifier(0.0);
+
+  void _onPageChanged(int newPage) {
+    setState(() {
+      _currentPage = newPage;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController.addListener(() {
+      if (_pageController.page == null) return;
+      _scroll.value = _pageController.page!;
+    });
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 300,
-      width: double.infinity,
       child: Stack(
         children: [
-          Swiper(
-            scale: 0.9,
-            //viewportFraction: 0.9,
+          AnimatedSwitcher(
+            duration: Duration(milliseconds: 500),
+            child: Container(
+              key: ValueKey(_currentPage),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(posterList[_currentPage]),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: BackdropFilter(
+                filter: ImageFilter.blur(
+                  sigmaX: 5,
+                  sigmaY: 5,
+                ),
+                child: Container(
+                  color: Colors.black.withOpacity(0.5),
+                ),
+              ),
+            ),
+          ),
+          PageView.builder(
+            onPageChanged: _onPageChanged,
+            controller: _pageController,
             itemCount: posterList.length,
-            autoplay: true,
-            itemBuilder: (BuildContext context, int index) {
-              return Stack(
+            //scrollDirection: Axis.horizontal,
+            itemBuilder: (context, index) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Image.asset(
-                    posterList[index],
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                  ),
-                  BackdropFilter(
-                    filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
-                    child: Container(
-                      color: Colors.black.withOpacity(0.5),
-                    ),
+                  ValueListenableBuilder(
+                    valueListenable: _scroll,
+                    builder: (context, scroll, child) {
+                      final difference = (scroll - index).abs();
+                      final scale = 1 - (difference * 0.2);
+                      return Transform.scale(
+                        scale: scale,
+                        child: Container(
+                          height: 254.5,
+                          width: 180,
+                          decoration: BoxDecoration(
+                            color: Colors.red,
+                            borderRadius: BorderRadius.circular(10),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.4),
+                                blurRadius: 10,
+                                spreadRadius: 2,
+                                offset: Offset(0, 8),
+                              ),
+                            ],
+                            image: DecorationImage(
+                              image: AssetImage(posterList[index]),
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ),
+                      );
+                    },
                   ),
                 ],
               );
             },
-          ),
-          SizedBox(
-            //color: AppColors.grey, //이걸로 사이즈 확인
-            //height: 350,
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(0, 30, 0, 0),
-              child: Swiper(
-                outer: true,
-                itemCount: posterList.length,
-                viewportFraction: 0.6,
-                // 위젯크기(포스터 크기)
-                scale: 0.8,
-                // 사진간의 간격
-                pagination: const SwiperPagination(
-                  alignment: Alignment.bottomCenter,
-                  margin: EdgeInsets.all(10),
-                  builder: DotSwiperPaginationBuilder(
-                    size: 5.0,
-                    space: 3.0,
-                    activeColor: AppColors.brightBlue,
-                    color: AppColors.middleGrey,
-                  ),
-                ),
-                autoplay: true,
-                autoplayDisableOnInteraction: true,
-                // autoplay is disabled when use swipes.
-                itemBuilder: (BuildContext context, int index) {
-                  return Image.asset(posterList[index]);
-                }, //control: SwiperControl(),
-              ),
-            ),
-          ),
+          )
         ],
       ),
     );
