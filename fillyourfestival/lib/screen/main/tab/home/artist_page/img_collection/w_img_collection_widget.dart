c0fef1b9559639ac3932b78022a7b4f85d879435
Jaehoon Yang <lljhms@naver.com>
2023-10-23 20:46:12 +0900

    add image collection page

--- fillyourfestival/lib/screen/main/tab/home/artist_page/img_collection/w_img_collection_widget.dart
+++ fillyourfestival/lib/screen/main/tab/home/artist_page/img_collection/w_img_collection_widget.dart
@@ -17,6 +17,7 @@ class _ImgCollectionWidgetState extends State<ImgCollectionWidget> {
   Widget build(BuildContext context) {
     return ListView.builder(
       shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
       itemCount: 10,
       itemBuilder: (context, index) {
         return Container(
