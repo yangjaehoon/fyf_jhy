80c789079cac6d5b2956b36df1497921d61132ed
yangjaehoon <lljhms@naver.com>
2024-07-28 19:46:15 +0900

    kakao login 버튼 전까지 추가

--- fillyourfestival/lib/login/login.dart
+++ fillyourfestival/lib/login/login.dart
@@ -139,9 +139,10 @@
 //   }
 // }
 
-
 import 'package:fast_app_base/login/signup.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'package:provider/provider.dart';
 import 'package:google_fonts/google_fonts.dart';
 import '../controller/auth_provider.dart';
@@ -232,7 +233,8 @@ class LoginPage extends StatelessWidget {
                   onTap: () async {
                     try {
                       await Provider.of<AuthProvider>(context, listen: false)
-                          .login(emailController.text.trim(), passwordController.text.trim());
+                          .login(emailController.text.trim(),
+                              passwordController.text.trim());
                     } catch (e) {
                       ScaffoldMessenger.of(context).showSnackBar(
                         SnackBar(content: Text('Login failed: $e')),
@@ -284,5 +286,70 @@ class LoginPage extends StatelessWidget {
         ),
       ),
     );
+
+
+  }
+
+  Widget getKakaoLoginButton() {
+    return InkWell(
+      onTap: (){
+        signInWithKakao();
+      },
+      child: Card(
+        margin: const EdgeInsets.fromLTRB(20,20,20,0),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(7)),
+        elevation: 2,
+        child: Container(
+          height: 50,
+          decoration: BoxDecoration(
+            color: Colors.yellow,
+            borderRadius: BorderRadius.circular(7),
+          ),
+          child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Image.asset('kakao_login_medium_narrow.png',height: 30),
+            const SizedBox(
+              width: 10,
+            ),
+            const Text("Sign In with Kakao",
+            style: TextStyle(color: Colors.white, fontSize:17))
+          ],),
+        ),
+      ),
+    );
+  }
+
+  Future<void> signInWithKakao() async {
+    // 카카오 로그인 구현 예제
+
+    // 카카오톡 실행 가능 여부 확인
+    // 카카오톡 실행이 가능하면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
+    if (await isKakaoTalkInstalled()) {
+      try {
+        await UserApi.instance.loginWithKakaoTalk();
+        print('카카오톡으로 로그인 성공');
+      } catch (error) {
+        print('카카오톡으로 로그인 실패 $error');
+
+        // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+        // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+        if (error is PlatformException && error.code == 'CANCELED') {
+          return;
+        }
+        // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인
+        try {
+          await UserApi.instance.loginWithKakaoAccount();
+          print('카카오계정으로 로그인 성공');
+        } catch (error) {
+          print('카카오계정으로 로그인 실패 $error');
+        }
+      }
+    } else {
+      try {
+        await UserApi.instance.loginWithKakaoAccount();
+        print('카카오계정으로 로그인 성공');
+      } catch (error) {
+        print('카카오계정으로 로그인 실패 $error');
+      }
+    }
   }
 }
--- fillyourfestival/lib/main.dart
+++ fillyourfestival/lib/main.dart
@@ -1,79 +1,3 @@
-// import 'dart:developer';
-//
-// import 'package:easy_localization/easy_localization.dart';
-// import 'package:fast_app_base/common/cli_common.dart';
-// import 'package:fast_app_base/common/common.dart';
-// import 'package:fast_app_base/provider/poster/poster_provider.dart';
-// import 'package:fast_app_base/provider/user_provider.dart';
-// import 'package:flutter/material.dart';
-// import 'package:fast_app_base/login/login.dart';
-// import 'package:firebase_core/firebase_core.dart';
-// import 'package:fast_app_base/controller/auth_provider.dart';
-// import 'package:flutter_naver_map/flutter_naver_map.dart';
-// import 'package:get/get.dart';
-// import 'package:provider/provider.dart';
-// import 'app.dart';
-// import 'auth/get_api_key.dart';
-// import 'common/data/preference/app_preferences.dart';
-//
-// import 'package:flutter_native_splash/flutter_native_splash.dart';
-//
-// Future<void> _initializeNaverMap() async {
-//   final naveMapApiKey = await getApiKey("naver_map_client_id");
-//   await NaverMapSdk.instance.initialize(
-//     clientId: naveMapApiKey,
-//     onAuthFailed: (e) => log("네이버맵 인증오류 : $e", name: "onAuthFailed"),
-//   );
-// }
-//
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await _initializeNaverMap();
-//   final bindings = WidgetsFlutterBinding.ensureInitialized();
-//   FlutterNativeSplash.preserve(widgetsBinding: bindings);
-//   //await Firebase.initializeApp().then((value) => Get.put(AuthController()));
-//   await EasyLocalization.ensureInitialized();
-//   await AppPreferences.init();
-//   runApp(
-//     EasyLocalization(
-//       supportedLocales: const [Locale('en'), Locale('ko')],
-//       fallbackLocale: const Locale('en'),
-//       path: 'assets/translations',
-//       useOnlyLangCode: true,
-//       child: MultiProvider(
-//         providers: [
-//           ChangeNotifierProvider(create: (context) => AuthProvider()),
-//           ChangeNotifierProvider<PosterProvider>(
-//               create: (context) => PosterProvider()),
-//           ChangeNotifierProvider<UserProvider>(
-//               create: (context) => UserProvider()),
-//         ],
-//         child: const MyApp(),
-//       ),
-//     ),
-//   );
-// }
-//
-// class MyApp extends StatelessWidget {
-//   const MyApp({Key? key}) : super(key: key);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Consumer<AuthProvider>(
-//         builder: (context, auth, _) {
-//           if (auth.user == null) {
-//             return LoginPage();
-//           } else {
-//             return App();
-//           }
-//         },
-//       ),
-//     );
-//   }
-// }
-
-
 import 'dart:developer';
 
 import 'package:easy_localization/easy_localization.dart';
@@ -86,6 +10,7 @@ import 'package:fast_app_base/login/login.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:fast_app_base/controller/auth_provider.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'package:provider/provider.dart';
 import 'app.dart';
 import 'auth/get_api_key.dart';
@@ -105,12 +30,20 @@ Future<void> _initializeNaverMap() async {
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  await _initializeNaverMap();
+
   final bindings = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: bindings);
+
+  await Firebase.initializeApp();
+  await _initializeNaverMap();
   await EasyLocalization.ensureInitialized();
   await AppPreferences.init();
+
+  KakaoSdk.init(
+    nativeAppKey: await getApiKey("kakao_native_app_key"),
+    javaScriptAppKey: await getApiKey("kakao_javaScript_app_key"),
+  );
+
   runApp(
     EasyLocalization(
       supportedLocales: const [Locale('en'), Locale('ko')],
